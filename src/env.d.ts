c218c1da5eb10c847415c9c2ad47c7dd1cbdf7f1
Saksham Goel <saksham.goel29@gmail.com>
2026-06-26 14:31:32 -0400

    feat: update footer carbon badge handling for local development, improve header import syntax, and adjust index page structure with main tag

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,9 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+interface ImportMeta {
+  readonly glob: <T = unknown>(
+    pattern: string,
+    options?: { eager?: boolean; import?: string; query?: string; as?: string }
+  ) => Record<string, T>;
+}
